087ac1269cb581146df021b7ea2958710cd1d403
Jay Beitsma <s1214179@student.windesheim.nl>
2025-04-07 16:55:48 +0200

    Forgot to change this during last test

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,24 +2,26 @@
     <div class="container mx-auto flex items-center justify-between h-full px-4 md:px-8">
         <!-- Navigation -->
         <x-nav/>
-        @auth('admin')
-            {{-- Admin is logged in --}}
-            <x-dashboard-points :username="auth('admin')->user()->name" :points="auth('admin')->user()->points"/>
-            <form method="POST" action="{{ route('admin.logout') }}">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
-        @elseauth
-            {{-- User is logged in --}}
-            <x-dashboard-points :username="auth()->user()->name" :points="auth()->user()->points"/>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
-        @else
-            {{-- Guest navigation --}}
-            <x-login-register/>
-        @endauth
+        <div class="flex justify-between items-center w-1/4">
+            @auth('admin')
+                {{-- Admin is logged in --}}
+                <x-dashboard-points :username="auth('admin')->user()->name" :points="auth('admin')->user()->points"/>
+                <form method="POST" action="{{ route('admin.logout') }}">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            @elseauth
+                {{-- User is logged in --}}
+                <x-dashboard-points :username="auth()->user()->name" :points="auth()->user()->points"/>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            @else
+                {{-- Guest navigation --}}
+                <x-login-register/>
+            @endauth
+        </div>
     </div>
 </header>
 
